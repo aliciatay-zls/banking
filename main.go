86a0f434eb01a72b6a6fed4483ab00903125abd3
aliciatay-zls <e0424736@u.nus.edu>
2022-12-04 23:57:54 +0800

    Create customers route, marshal struct response to json

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,12 +9,20 @@ import (
 
 /*
 1. Create a "greet" route that sends back "Hello world!" as response, start and run server
+2. JSON encoding of response for "customers" route
 */
+
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	Zipcode string `json:"zipCode"`
+}
 
 func main() {
 	//routes
 	//register handler function (handles responses) for the pattern (url) in the DefaultServeMux (router)
 	http.HandleFunc("/greet", greetHandler)
+	http.HandleFunc("/customers", customersHandler)
 
 	//start and run server
 	//listen on localhost and pass DefaultServeMux (default handler) to Serve()
@@ -26,4 +35,18 @@ func main() {
 //sends back "Hello world!" as response
 func greetHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
+}
+
+//returns a json response
+func customersHandler(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Dorothy", "Emerald City", "12345"},
+		{"Luke", "Tatooine", "67890"},
+	}
+
+	//modify response header (otherwise response will still be in application/text form even though encode in json)
+	//return Header object of the ResponseWriter w, add to it this key-value pair
+	w.Header().Add("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(customers)
 }
